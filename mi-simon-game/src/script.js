5f9698658fe59a6e6a5036b9d0e223374686f483
Diego-Brito05 <britodiegoc@gmail.com>
2025-02-11 23:04:53 -0400

    Brillo del boton al presionarlo y avances con el efecto de sonido

--- mi-simon-game/src/script.js
+++ mi-simon-game/src/script.js
@@ -1,23 +1,27 @@
-import './App.css';
+// Crear los objetos de los sonidos
+const sounds = {
+  green: new Audio('sounds/GreenButtonSound.wav'),
+  red: new Audio('sounds/RedButtonSound.wav'),
+  yellow: new Audio('sounds/YellowButtonSound.wav'),
+  blue: new Audio('sounds/BlueButtonSound.wav')
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Obtener los botones
+const greenButton = document.getElementById('green');
+const redButton = document.getElementById('red');
+const yellowButton = document.getElementById('yellow');
+const blueButton = document.getElementById('blue');
 
-export default App;
+// Asignar los eventos de clic para reproducir los sonidos
+greenButton.addEventListener('click', () => {
+  sounds.green.play();
+});
+redButton.addEventListener('click', () => {
+  sounds.red.play();
+});
+yellowButton.addEventListener('click', () => {
+  sounds.yellow.play();
+});
+blueButton.addEventListener('click', () => {
+  sounds.blue.play();
+});
